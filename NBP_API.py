7486ce71e65d4c78e0317fc8dfb0ebcdb257f891
Kacper <kacper-cyganik@o2.pl>
2022-06-07 19:04:23 +0200

    Rename nbp_api -> NBP_API.

---
+++ NBP_API.py
@@ -0,0 +1,24 @@
+import requests
+import logging
+
+
+class NBP_API:
+    '''
+    Class providing a method to fetch current exchange rate of currency of user chocice.
+    '''
+    def __init__(self) -> None:
+        self._base_url = 'http://api.nbp.pl/api/exchangerates/rates/a/'
+        self._logger = logging.getLogger("my logger")
+
+    def get_currency(self, currency_code: str) -> float:
+        '''
+        Get current exchange rate of given currency. Takes currency code as argument (like 'usd' for dollar & 'eur' for euro). Returns float value of exchange rate or None if error occures.
+        '''
+        endpoint = self._base_url+currency_code+'/'
+        try:
+            response = requests.get(endpoint)
+            self._logger.info(f'Successful fetch at {endpoint}')
+            return float(response.json()['rates'][0]['mid'])
+        except requests.exceptions.RequestException as e:
+            self._logger.error(f'Failed fetch at {endpoint} - {e}')
+            return None
